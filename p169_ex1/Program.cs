f7cb6b78ee7fe959c2b5dd4869a8e85669eb7c7e
JINNY <sousaca05@gmail.com>
2023-03-15 20:07:53 +0900

    230315_home

---
+++ p169_ex1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace p169_ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i =0; i <5; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
