dcb2ff1f47abea0d74c00ae8b8471611ad159c47
JINNY <sousaca05@gmail.com>
2023-03-17 15:36:10 +0900

    230317_school

---
+++ Break_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Break_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = true;
+            
+            while (a)
+            {
+                Console.Write("계속할까요?(예/아니오) : ");
+                string answer = Console.ReadLine();
+
+                if (answer == "아니오")
+                {
+                  
+                    }
+                else
+                {
+
+                }
+
+
+
+                }
+            }
+        }
+    }
+}
